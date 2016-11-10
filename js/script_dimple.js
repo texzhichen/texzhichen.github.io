64667ac8aad8e536c957c17877dfa2bf92c70e8a
zhichen <zxia31@gatech.edu>
2016-11-09 22:17:09 -0500

    Hide/show charts implemented

--- js/script_dimple.js
+++ js/script_dimple.js
@@ -50,42 +50,81 @@ function title(svg, title) {
         });
 }
 
-var svg = dimple.newSvg("#chartContainer", larghezzaChart, altezzaChart);
-var data = [
-    {
-        "Club" : "A",
-        "Attack" : 100,
-        "Defense" : 80,
-        "P" : 0.7
-    },
-    {
-        "Club" : "B",
-        "Attack" : 80,
-        "Defense" : 60,
-        "P" : 0.6
-    },
-    {
-        "Club" : "C",
-        "Attack" : 60,
-        "Defense" : 100,
-        "P" : 0.5
-    },
-];
-var myChart = new dimple.chart(svg, data);
-myChart.addCategoryAxis("x", "Attack");
-myChart.addCategoryAxis("y", "Defense");
-myChart.addMeasureAxis("z", "P");
-myChart.addSeries("Club", dimple.plot.bubble);
-myChart.addLegend(60, 10, 510, 20, "right");
-myChart.draw();
 
-var svg = dimple.newSvg("#chartContainer", larghezzaChart, altezzaChart);
-d3.tsv("/data/networth.tsv", function(data) {
-    var myChart = new dimple.chart(svg, data);
-    myChart.setBounds(75, 30, 480, 330)
-    myChart.addMeasureAxis("x", "Networth");
-    var y = myChart.addCategoryAxis("y", "Clubs");
-    y.addOrderRule("Date");
-    myChart.addSeries(null, dimple.plot.bar);
-    myChart.draw();
+// constants
+const minNumChart = 1;
+const maxNumChart = 10;
+
+// temporary variable declaration
+var i;
+var s;
+var oldVal;
+
+// create chart containers
+for (i = 0; i < maxNumChart; i++) {
+    s = "<div id=barchart" + i + "></div>";
+    $("#chart").append(s)    
+}
+
+// draw all then hide [minNumChart, maxNumChart) charts
+for (i = 0; i < maxNumChart; i++) {
+    s = "#barchart" + i;
+    svg = d3.select(s).append("svg").attr("width", 860).attr("height", 355);
+    drawBarChart(svg);
+}
+for (i = minNumChart; i < maxNumChart; i++) {
+    s = "#barchart" + i;
+    $(s).hide();
+}
+
+// show [0, event.value] charts
+$('#barchart').slider().on('slide', function(event){
+    for (i = 0; i < maxNumChart; i++) {
+        s = "#barchart" + i;
+        i < event.value ? $(s).show() : $(s).hide();
+    }
 });
+
+
+
+
+
+//var svg = dimple.newSvg("#chartContainer", larghezzaChart, altezzaChart);
+//var data = [
+//    {
+//        "Club" : "A",
+//        "Attack" : 100,
+//        "Defense" : 80,
+//        "P" : 0.7
+//    },
+//    {
+//        "Club" : "B",
+//        "Attack" : 80,
+//        "Defense" : 60,
+//        "P" : 0.6
+//    },
+//    {
+//        "Club" : "C",
+//        "Attack" : 60,
+//        "Defense" : 100,
+//        "P" : 0.5
+//    },
+//];
+//var myChart = new dimple.chart(svg, data);
+//myChart.addCategoryAxis("x", "Attack");
+//myChart.addCategoryAxis("y", "Defense");
+//myChart.addMeasureAxis("z", "P");
+//myChart.addSeries("Club", dimple.plot.bubble);
+//myChart.addLegend(60, 10, 510, 20, "right");
+//myChart.draw();
+//
+//var svg = dimple.newSvg("#chartContainer", larghezzaChart, altezzaChart);
+//d3.tsv("/data/networth.tsv", function(data) {
+//    var myChart = new dimple.chart(svg, data);
+//    myChart.setBounds(75, 30, 480, 330)
+//    myChart.addMeasureAxis("x", "Networth");
+//    var y = myChart.addCategoryAxis("y", "Clubs");
+//    y.addOrderRule("Date");
+//    myChart.addSeries(null, dimple.plot.bar);
+//    myChart.draw();
+//});
