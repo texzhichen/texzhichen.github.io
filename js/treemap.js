51f6bc20586bf98d81f62b9f4fd3f23d29745cca
Zheng, Chendong <chendong@gatech.edu>
2016-11-10 12:59:11 -0500

    动态读取属性
    tsv添加新的类型属性不用改代码

--- js/treemap.js
+++ js/treemap.js
@@ -1,30 +1,41 @@
 var TeamList=new Array(5);
 var TeamExample=["Real Madrid","Bacerlona","Atletico Madrid", "Bayern Munchen","Arsenal"];//Team example  球队例子
-var AttrExample=["NetWorth","TotalMarketValue"];//Attribute example   属性例子
-
-
+var AttrExample=["NetWorth","TotalMarketValue","GoalScored","wrongAttr"];//Attribute example   属性例子
+var tempValue
+var headerNames;
 d3.tsv("data/data.tsv", function(error, data) {
   if (error) throw error;
 
   //Coerce the data to numbers.
-  	
+     headerNames=d3.keys(data[0]);
+      console.log(headerNames);
 
   data.forEach(function(d)
   {
+
     TeamList[d.Number-1]=new Object();
 	TeamList[d.Number-1].Name=d.Name;
     // console.log(TeamList[d.Number-1].Name);
-	TeamList[d.Number-1].TotalMarketValue=+d.TotalMarketValue;
-    TeamList[d.Number-1].NetWorth=+d.NetWorth;
-    TeamList[d.Number-1].Place=d.Number-1;
-    console.log(TeamList[d.Number-1].Place);
-    console.log(TeamList[d.Number-1].Name);
+	// TeamList[d.Number-1].TotalMarketValue=+d.TotalMarketValue;
+    // TeamList[d.Number-1].NetWorth=+d.NetWorth;
+    // TeamList[d.Number-1].Place=d.Number-1;
+    // console.log(TeamList[d.Number-1].Place);
+    // console.log(TeamList[d.Number-1].Name);
+    TeamList[d.Number-1].attribute=new Array(headerNames.length-1);
+   
+    for(var i=0;i<headerNames.length;i++)
+    {
+        TeamList[d.Number-1].attribute[i]=new Object();
+        TeamList[d.Number-1].attribute[i].name=headerNames[i];
+        eval("tempValue=+d."+headerNames[i]);
+        TeamList[d.Number-1].attribute[i].value=tempValue;
+    }
+
     
   })
+  
+TeamList[1].attribute.forEach(function(d){console.log(d.name);})
 
-
-console.log(TeamList);
-console.log(TeamList.length);
 var tree = {
     name: "tree"
 };
@@ -69,21 +80,22 @@ function selectTeams(_inputTeam,_inputAttr) //input  teams[] and attributes[] he
 {
 	for(var i=0;i<_inputTeam.length;i++)
 	{
-        console.log(_inputTeam.length);
-        console.log(_inputTeam[i]);        
+          
         var Place=searchTeam(_inputTeam[i]); //search the input teams
-        console.log(Place);
-        console.log(TeamList[Place].Name);
+        // console.log(Place);
+        // console.log(TeamList[Place].Name);
         tree.children[i]=new Object();
         tree.children[i].name=TeamList[Place].Name;
         tree.children[i].size=0;
+        TeamList[Place].attribute.forEach(function(d){console.log(d.name);})
         for(var j=0;j<_inputAttr.length;j++)
         {
             tree.children[i].size+=returnAttrs(TeamList[Place],_inputAttr[j]); //sum value of attributes 计算总分
             
             tree.children[i].name=tree.children[i].name+"+"+returnAttrs(TeamList[Place],_inputAttr[j]);
         }
-        console.log(tree.children[i].size);
+        tree.children[i].name=tree.children[i].name=tree.children[i].name+"="+tree.children[i].size
+        // console.log(tree.children[i].size);
 	}
 }
 
@@ -101,19 +113,21 @@ function searchTeam(_inputTeamName) //search input team
     }
     
 }
-function returnAttrs(in_putTeam,_inputAttr) //get value of attributes
+function returnAttrs(_inputTeam,_inputAttr) //get value of attributes
 {
-    switch(_inputAttr)
+    var backValue=0;
+    console.log(_inputTeam.attribute[2].name);
+    for(var i=1;i<headerNames.length;i++)
     {
-        case "NetWorth":
-            return in_putTeam.NetWorth;
+        if(_inputTeam.attribute[i].name==_inputAttr)
+        {
+            backValue=_inputTeam.attribute[i].value;
+            
             break;
-        case "TotalMarketValue":
-            return in_putTeam.TotalMarketValue;
-            break;
-        default:
-        break;
+        }
     }
+    return backValue;
+    
 }
 
 
